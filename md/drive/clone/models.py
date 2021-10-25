c64663e052c79b5512aaaf18c16040c8d6030b3c
BluberryPickle <milinddinesh@gmail.com>
2021-10-25 21:49:14 +0530

    cleaned files

--- md/drive/clone/models.py
+++ md/drive/clone/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 # class FilesUpload(models.Model):
     # file = models.FileField()
+
+class Files(models.Model):
+    filename = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+    pdf = models.FileField(upload_to='books/pdfs/')
