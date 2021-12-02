0ab0560aa327af388c628675b9f0bcb42b780cff
BluberryPickle <milinddinesh@gmail.com>
2021-12-03 02:26:21 +0530

    renamed folder

--- Broken_auth/Broken_access/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Document(models.Model):
-    file = models.FileField(upload_to='')
-
-
-    def __str__(self):
-        return self.title
--- Broken_auth/Secure_Broken_Access/app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Document
-
-class UploadFileForm(forms.ModelForm):
-    class Meta:
-        model = Document
-        fields = ['file']
-        
